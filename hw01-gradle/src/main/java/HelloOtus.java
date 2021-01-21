e0ce69da80a3d556b892afeab0ebdb593cd2335c
Dmitriy-Berezutskiy <dmitr.berezutskiy@gmail.com>
2021-01-21 19:33:04 +0300

    add hw01

--- hw01-gradle/src/main/java/HelloOtus.java
+++ hw01-gradle/src/main/java/HelloOtus.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class HelloOtus {
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class HelloOtus {
+    public static void main(String... args) {
+        List<Integer> example = new ArrayList<>();
+        int min = 0;
+        int max = 500;
+        for (int i = min; i < max; i++) {
+            example.add(i);
+        }
+        System.out.println(Lists.reverse(example));
+        System.out.println(Lists.reverse(example));
+    }
 }
